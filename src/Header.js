9ec0f7f23a97ee5e585838e7d8a2af17450f754b
devmnl <dev.mnlfranco@gmail.com>
2024-10-14 23:44:54 -0300

    update

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Header.css';
 import logo from './logo-m.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faReact, faGithub, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -21,10 +23,27 @@ const Header = () => {
           <li><a href="#services">Services</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
+        {/* Seção de ícones sociais */}
+      <div className="social-icons">
+        <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faReact} />
+        </a>
+        <a href="https://github.com" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://wa.me/YOUR_WHATSAPP_NUMBER" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+      </div>
       </nav>
       <div className="menu-toggle" onClick={toggleMenu}>
         {menuOpen ? '✕' : '☰'} {/* Alterna entre o ícone de menu e fechar */}
       </div>
+      {/* Ícones das redes sociais */}
+      
     </header>
   );
 };
